3dcf694bd8c2be97543814854341c99ba018bfe5
frayfray248 <nicman248@hotmail.com>
2020-12-15 17:09:09 -0800

    canvas mouse event handlers

--- public/script.js
+++ public/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+
+    // mouse
+    var mouse = {
+        click: false,
+        move: false,
+        position: { x : 0, y : 0},
+        position_previous : false
+    }
+
+    // canvas
+    var canvas = $('#drawingCanvas');
+    var context = canvas[0].getContext('2d');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    // set canvas dimensions to browser dimensions
+    canvas[0].width = width;
+    canvas[0].height = height;
+
+    // canvas mouse event handlers
+    canvas.mousedown(function () {
+        console.log("mouse down on canvas");
+    })
+
+    canvas.mouseup(function () {
+        console.log("mouse up on canvas");
+    })
+
     var socket = io();
     
 });
